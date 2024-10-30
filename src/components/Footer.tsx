9a4f5bf2eea77fb18578b2748af6be5034a9b0b1
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-30 22:32:52 +0530

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <div className='h-[269px] w-full bg-[#214F70]'></div>
+    <div className='h-[269px] w-full bg-[#214F70] text-white flex justify-center items-center'>
+      <p>Made by <a href="https://x.com/fardeen14693425">Fardeen</a> </p>
+    </div>
   )
 }
 
