64464bb1b1a84e3b5ffe1f0cea36a738081c36d0
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-05 21:02:38 +0530

    fix: changes in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className='h-[269px] w-full bg-[#214F70] text-white flex justify-center items-center'>
-      <p>Made by <a href="https://x.com/fardeen14693425">Fardeen</a> </p>
+      <p>Made by <a className="underline" href="https://x.com/fardeen14693425">Fardeen</a> </p>
     </div>
   )
 }
